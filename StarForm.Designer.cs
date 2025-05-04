a45796081b07b4b80b780c73027c4d2926774963
Lera <sagwa996754821@gmail.com>
2025-05-04 14:34:28 +0300

    Add json files

--- StarForm.Designer.cs
+++ StarForm.Designer.cs
@@ -29,34 +29,146 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StarForm));
-            label1 = new Label();
+            panel1 = new Panel();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(274, 156);
-            label1.Name = "label1";
-            label1.Size = new Size(33, 20);
-            label1.TabIndex = 0;
-            label1.Text = "star";
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(697, 29);
+            panel1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Dock = DockStyle.Left;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(377, 0);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(187, 28);
+            comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Right;
+            button1.Location = new Point(603, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Пошук";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Left;
+            textBox1.Location = new Point(0, 0);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(377, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 35);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(673, 322);
+            dataGridView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 385);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "Додати";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(237, 385);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "Видалити";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(127, 385);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 5;
+            button4.Text = "Редагувати";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(515, 385);
+            button5.Name = "button5";
+            button5.Size = new Size(170, 29);
+            button5.TabIndex = 6;
+            button5.Text = "Оновити таблицю";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(12, 420);
+            button6.Name = "button6";
+            button6.Size = new Size(319, 29);
+            button6.TabIndex = 7;
+            button6.Text = "Згенерувати флеш-картки";
+            button6.UseVisualStyleBackColor = true;
             // 
             // StarForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(697, 803);
-            Controls.Add(label1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "StarForm";
             Text = "StarForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
+        private Panel panel1;
+        private Button button1;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private ComboBox comboBox1;
+        private Button button5;
+        private Button button6;
     }
 }
